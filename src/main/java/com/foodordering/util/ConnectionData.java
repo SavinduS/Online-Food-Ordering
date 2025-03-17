64bafd058dec3cfbcf4f87a81203dfce25e0948e
SavinduS <savinduweerarathna@gmail.com>
2025-03-17 23:17:11 +0530

    first commit

---
+++ src/main/java/com/foodordering/util/ConnectionData.java
@@ -0,0 +1,8 @@
+package com.foodordering.util;
+
+public class ConnectionData {
+
+    public static final String DATABASE = "foodordering";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "123456";
+}
